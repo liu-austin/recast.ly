7ffd5f4fc9734d76a59f6c790c230597b1205f14
liu-austin <austinliu279@gmail.com>
2019-11-04 17:36:42 -0800

    initial commit

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,14 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {
+      props.videos.map((video, index) => {
+        return (
+          <VideoListEntry key={index} video={video} onVideoListClick={props.onVideoListClick}/>
+        );
+      })
+    }
   </div>
 );
 
