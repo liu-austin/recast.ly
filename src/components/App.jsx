6d179b38fc57893602487651b0997ed0a958e52c
liu-austin <austinliu279@gmail.com>
2019-11-04 21:05:40 -0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,35 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import searchYoutube from '../lib/searchYouTube.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      selectedTitle: null,
-      selectedDescription: null,
-      selectedUrl: null
+      selectedTitle: exampleVideoData[0].snippet.title,
+      selectedDescription: exampleVideoData[0].snippet.description,
+      selectedUrl: `https://www.youtube.com/embed/${exampleVideoData[0].id.videoId}`,
+      selectedSearchTerm: ''
     };
+    this.onVideoListClick = this.onVideoListClick.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  onVideoListClick() {
+  onVideoListClick(e) {
+    this.setState({selectedTitle: e.target.innerHTML});
+    var selectedVideo = exampleVideoData.filter(video => {
+      return video.snippet.title === e.target.innerHTML;
+    });
+    this.setState({selectedDescription: selectedVideo[0].snippet.description});
+    this.setState({selectedUrl: `https://www.youtube.com/embed/${selectedVideo[0].id.videoId}`});
+  }
 
+  onSearchSubmit(e) {
+    var input = document.getElementById('form-control');
+    this.setState({selectedSearchTerm: input.value});
+    // searchYoutube()
   }
 
   render() {
@@ -22,15 +38,15 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <Search search={this.onSearchSubmit}/>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={exampleVideoData[0]} />
+            <VideoPlayer video={{url: this.state.selectedUrl, title: this.state.selectedTitle, description: this.state.selectedDescription}} />
           </div>
           <div className="col-md-5">
-            <VideoList videos={exampleVideoData} onVideoListClick={onVideoListClick}/>
+            <VideoList videos={exampleVideoData} onVideoListClick={this.onVideoListClick}/>
           </div>
         </div>
       </div>
@@ -41,3 +57,10 @@ class App extends React.Component {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+// onVideoListClick={onVideoListClick}
+
+// Put your YouTube API keys here!
+// var YOUTUBE_API_KEY = 'AIzaSyA4cJQP24oJLe7ZYIgVGKDjhHCS7MrINbM';
+
+// export default YOUTUBE_API_KEY;
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,32 @@
 var searchYouTube = (options, callback) => {
   // TODO
+//   $.ajax({
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     query: options.q,
+//       max: options.maxResults,
+//         key: options.key,
+//   }, callback)
+//
 };
 
 export default searchYouTube;
+
+
+// readAll: function(successCB, errorCB = null) {
+//   $.ajax({
+//     url: "https://www.googleapis.com/youtube/v3/search",
+//     type: 'GET',
+//     data: { query: '-createdAt' },
+//     contentType: 'application/json',
+//     success: successCB,
+//     error: errorCB || function(error) {
+//       console.error('chatterbox: Failed to fetch messages', error);
+//     }
+//   });
+// }
+
+// $.get("https://www.googleapis.com/youtube/v3/channels", {
+//     part: "contentDetails",
+//     $("#subtype").val(): $("#url").val(),
+//     key: "MY-API-KEY"
+// }, callback())
